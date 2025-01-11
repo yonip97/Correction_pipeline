e8dee393ee590fdf58eda5d0417c479b7eceb16f
yonip97 <70665101+yonip97@users.noreply.github.com>
2025-01-11 11:42:15 +0200

    Update constants.py

--- DeFacto/dataset/constants.py
+++ DeFacto/dataset/constants.py
@@ -4,6 +4,6 @@ MODEL_PRICE_MAP = {'gpt-4o': {'input': 5, 'output': 15},
                        'claude-3-5-sonnet-20241022': {'input': 3, 'output': 15},
                        'gemini-1.5-pro': {'input': 1.5, 'output': 5},
                        "llama-3-1-70b-instruct": {'input': 1.8, 'output': 1.8},
-                       'llama-3-405b-instruct': {'input': 5, 'output': 16}}
+                       'llama3.1-405b': {'input': 3.6, 'output': 3.6}}
 
 DTYPE_MAP = {'float16': torch.float16, 'float32': torch.float32, 'bfloat16': torch.bfloat16}
