dc507eeecad05ed4dce64164c348decffd19c413
yonip97 <70665101+yonip97@users.noreply.github.com>
2025-03-01 14:05:47 +0200

    Update model_evaluation_with_llm_as_a_judge.py

--- DeFacto/dataset/model_evaluation_with_llm_as_a_judge.py
+++ DeFacto/dataset/model_evaluation_with_llm_as_a_judge.py
@@ -1,11 +1,10 @@
 import os.path
 
-from call_llms import ModelCaller, OpenAICaller, AnthropicCaller, WatsonCaller, GeminiCaller
 import argparse
 import pandas as pd
 from tqdm import tqdm
 from constants import MODEL_PRICE_MAP as model_price_map, DTYPE_MAP as dtype_map
-
+from inference_utils import chose_model
 
 def parse_args():
     args = argparse.ArgumentParser()
@@ -13,22 +12,20 @@ def parse_args():
     args.add_argument('-prompt_type', type=str)
     args.add_argument('-model_being_evaluated', type=str)
     args.add_argument('-llm_as_a_judge_dir', type=str)
+    args.add_argument('-output_path', type=str)
     args.add_argument('-model', type=str, default='meta-llama/Meta-Llama-3.1-8B-Instruct')
     args.add_argument('-prompt_path', type=str)
     args.add_argument('-past_text_prompt_path', type=str)
-    args.add_argument('-max_new_tokens', type=int, default=1000)
+    args.add_argument('-max_new_tokens', type=int, default=2000)
     args.add_argument('-device_map', type=str)
     args.add_argument('-dtype', type=str)
     args.add_argument('-data_path', type=str)
+    args.add_argument('-only_dev', action='store_true')
     args.add_argument('-llm_output_path', type=str)
     args.add_argument('-num_of_samples', type=int)
     args.add_argument('-api_key', type=str)
-    args.add_argument('-watson', action='store_true')
     args.add_argument('-azure', action='store_true')
     args.add_argument('-temp_save_dir', type=str)
-    args.add_argument('-iam_token', type=str)
-    args.add_argument('-hf_token', type=str)
-    args.add_argument('-project_id', type=str)
     args.add_argument('-sample_id', type=int)
     args.add_argument('-seperator', type=str)
     args = args.parse_args()
@@ -52,21 +49,24 @@ def parse_args():
             args.past_text_prompt = args.past_text_prompt.strip()
     else:
         args.past_text_prompt = ''
-    prompt_types = ['few_shot', 'zero_shot', 'cot']
-    if args.prompt_type not in prompt_types:
-        raise ValueError(f'Prompt type must be one of {prompt_types}')
-    args.output_path = os.path.join(args.llm_as_a_judge_dir, args.prompt_type)
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
-    args.output_path = os.path.join(args.output_path, args.model_being_evaluated)
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
-    args.output_path = os.path.join(args.output_path, args.output_file)
+    if args.output_path is None:
+        prompt_types = ['few_shot', 'zero_shot', 'cot']
+        if args.prompt_type not in prompt_types:
+            raise ValueError(f'Prompt type must be one of {prompt_types}')
+        args.output_path = os.path.join(args.llm_as_a_judge_dir, args.prompt_type)
+        if not os.path.exists(args.output_path):
+            os.makedirs(args.output_path)
+        args.output_path = os.path.join(args.output_path, args.model_being_evaluated)
+        if not os.path.exists(args.output_path):
+            os.makedirs(args.output_path)
+        args.output_path = os.path.join(args.output_path, args.output_file)
     return args
 
 
 def get_data(args):
     df = pd.read_csv(args.data_path, encoding='utf-8')
+    if args.only_dev:
+        df = df[df['set'] == 'dev']
     df = df[['text', 'model_summary', 'descriptions']]
     df.drop_duplicates(inplace=True)
     df2 = pd.read_csv(args.llm_output_path, encoding='utf-8')
@@ -93,23 +93,23 @@ def get_llm_outputs(args):
     return outputs
 
 
-def chose_model(args):
-    if 'gpt' in args.model:
-        return OpenAICaller(model=args.model, azure=args.azure, temp_save_dir=args.temp_save_dir,
-                            input_price=args.input_price, output_price=args.output_price)
-    elif 'gemini' in args.model:
-        return GeminiCaller(model=args.model, temp_save_dir=args.temp_save_dir,
-                            input_price=args.input_price,
-                            output_price=args.output_price)
-    elif 'claude' in args.model:
-        return AnthropicCaller(model=args.model, temp_save_dir=args.temp_save_dir,
-                               input_price=args.input_price, output_price=args.output_price)
-    elif args.watson:
-        return WatsonCaller(model=args.model, temp_save_dir=args.temp_save_dir,
-                            input_price=args.input_price, output_price=args.output_price)
-    else:
-        return ModelCaller(model_id=args.model, device_map=args.device_map,
-                           torch_dtype=args.dtype)
+# def chose_model(args):
+#     if 'gpt' in args.model:
+#         return OpenAICaller(model=args.model, azure=args.azure, temp_save_dir=args.temp_save_dir,
+#                             input_price=args.input_price, output_price=args.output_price)
+#     elif 'gemini' in args.model:
+#         return GeminiCaller(model=args.model, temp_save_dir=args.temp_save_dir,
+#                             input_price=args.input_price,
+#                             output_price=args.output_price)
+#     elif 'claude' in args.model:
+#         return AnthropicCaller(model=args.model, temp_save_dir=args.temp_save_dir,
+#                                input_price=args.input_price, output_price=args.output_price)
+#     elif args.watson:
+#         return WatsonCaller(model=args.model, temp_save_dir=args.temp_save_dir,
+#                             input_price=args.input_price, output_price=args.output_price)
+#     else:
+#         return ModelCaller(model_id=args.model, device_map=args.device_map,
+#                            torch_dtype=args.dtype)
 
 
 def get_sample(args):
@@ -123,7 +123,7 @@ def get_sample(args):
 
 def main():
     args = parse_args()
-    model = chose_model(args)
+    model = chose_model(args.model,args)
     llm_outputs, descriptions, texts, summaries = get_data(args)
     outputs = []
     errors = []
